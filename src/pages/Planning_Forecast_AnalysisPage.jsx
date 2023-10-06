323817055eb835624e9f6f1e817a27564bf8cd0c
Anupab <AnuphapKarezo@gmail.com>
2023-10-06 08:36:38 +0700

    Update Planning_Forecast_AnalysisPage R2

--- src/pages/Planning_Forecast_AnalysisPage.jsx
+++ src/pages/Planning_Forecast_AnalysisPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 // import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { DataGridPro } from '@mui/x-data-grid-pro';
 import { randomTraderName, randomEmail } from '@mui/x-data-grid-generator';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
@@ -9,7 +10,10 @@ import './Planning_Forecast_AnalysisPage.css'; // Import the CSS file
 import CircularProgress from '@mui/material/CircularProgress';
 // import { Axios } from "axios";
 import axios from "axios";
-
+import ConnectedTvTwoToneIcon from '@mui/icons-material/ConnectedTvTwoTone';
+import LaptopTwoToneIcon from '@mui/icons-material/LaptopTwoTone';
+import GppGoodIcon from '@mui/icons-material/GppGood';
+import GppMaybeIcon from '@mui/icons-material/GppMaybe';
 
 const columns = [
   { field: 'sales', headerName: 'Sales', width: 120 , headerAlign: 'center' , headerClassName: 'bold-header'},
@@ -19,9 +23,22 @@ const columns = [
   { field: 'cr', headerName: 'CR', width: 140 , headerAlign: 'center'  , headerClassName: 'bold-header'},
   { field: 'fc', headerName: 'FC', width: 80 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
   { field: 'po_cover_fc', headerName: 'PO-Cover-FC (WK)', width: 150 , headerAlign: 'center', align: 'center'  , headerClassName: 'bold-header'},
-  { field: 'fc_accuracy', headerName: 'FC_Accuracy', width: 150 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
+  { field: 'fc_accuracy', headerName: 'FC_Accuracy', width: 150 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header',
+      renderCell: (params) => (
+        <div>
+          {params.value}&nbsp;%&nbsp;&nbsp;&nbsp;&nbsp;
+          {params.row.fc_accuracy >= 70 ? (
+            <GppGoodIcon style={{ color: 'green' }} /> // Replace with the icon for Desktop
+          ) : (
+            <GppMaybeIcon style={{ color: '#E9B824' }} /> // Replace with the icon for Notebook
+          )}
+        </div>
+      ),
+  },
   { field: 'wip', headerName: 'WIP', width: 95 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
+  { field: 'wip_pending', headerName: 'WIP Pending (1.1,3.1)', width: 165 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
   { field: 'fg', headerName: 'FG', width: 95 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
+  { field: 'fg_unm', headerName: 'FG Unmovement', width: 140 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
   { field: 'po_bal', headerName: 'PO_BAL', width: 95 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
   { field: 'wip_fg_compare_po', headerName: 'WIP+FG compare PO', width: 175 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
   { field: 'wip_fg_compare_fc', headerName: 'WIP+FG compare FC', width: 175 , headerAlign: 'center' , align: 'center' , headerClassName: 'bold-header'},
@@ -77,7 +94,7 @@ export default function Planning_Forecast_AnalysisPage({ onSearch }) {
 
   const [columnVisibilityModel, setColumnVisibilityModel] = React.useState({});
   return (
-    <div className="table-responsive table-fullscreen" style={{ height: 800, width: '1750px' , marginTop: '5px' }}>
+    <div className="table-responsive table-fullscreen" style={{ height: 800, width: '2000px' , marginTop: '5px' }}>
       {isLoading ? ( // Render the loading indicator if isLoading is true
         <div className="loading-indicator" style={{display: 'flex' , flexDirection: 'column' , justifyContent: 'center' , alignItems: 'center' , height: '50vh'}}>
                 <CircularProgress /> {/* Use the appropriate CircularProgress component */}
@@ -111,8 +128,12 @@ export default function Planning_Forecast_AnalysisPage({ onSearch }) {
               rows={distinctFcAnalysis.map((row) => ({
                   ...row,
                   fc: formatNumberWithCommas(row.fc),
+                  // fc_accuracy: formatNumberWithCommas(parseInt(row.fc_accuracy, 10)) ,
+                  // fc: `${formatNumberWithCommas(row.fc)}%`,
                   wip: formatNumberWithCommas(row.wip),
+                  wip_pending: formatNumberWithCommas(row.wip_pending),
                   fg: formatNumberWithCommas(row.fg),
+                  fg_unm: formatNumberWithCommas(row.fg_unm),
                   po_bal: formatNumberWithCommas(row.po_bal),
               }))}
               // disableColumnFilter
@@ -122,7 +143,7 @@ export default function Planning_Forecast_AnalysisPage({ onSearch }) {
               onFilterModelChange={(newModel) => setFilterModel(newModel)}
               slotProps={{ toolbar: { showQuickFilter: true } }}
               columnVisibilityModel={columnVisibilityModel}
-              checkboxSelection
+              // checkboxSelection
               onColumnVisibilityModelChange={(newModel) =>
                 setColumnVisibilityModel(newModel)
               }
